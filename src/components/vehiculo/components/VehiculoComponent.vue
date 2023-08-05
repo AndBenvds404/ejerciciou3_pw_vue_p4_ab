f2d726b1dc348caa896546b04220f4a4fb7755ba
un <rabit003a@gmail.com>
2023-08-05 10:54:19 -0500

    pagina web ejercicio

---
+++ src/components/vehiculo/components/VehiculoComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  
+    <table style="width:100%">
+    <tr>
+        <th>id</th>
+        <th>placa</th>
+        <th>marca</th>
+        <th>color</th>
+        <th>año</th>
+    </tr>
+    <tr>
+        <td ></td>
+        <td ></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    </table>
+
+</template>
+
+<script>
+
+export default {
+
+    Data:{
+        
+        
+
+    },
+    method:{
+        
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/vehiculo/pages/VehiculoPage.vue
@@ -0,0 +1,20 @@
+<template>
+  
+  <VehiculoComponent/>
+</template>
+
+<script>
+
+import VehiculoComponent from '../components/VehiculoComponent.vue';
+
+
+export default {
+    components: {
+        VehiculoComponent
+    },
+}
+</script>
+
+<style>
+
+</style>
